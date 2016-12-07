92c67f7129f1d7cabd76a5b52fd70dfab7ede76a
Programador <Programador@10.41.1.129>
2016-12-07 14:51:52 -0200

    comentado a linha do diretório

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -4,7 +4,7 @@ class HelloTest extends Silex\WebTestCase
 {
     public function createApplication()
     {
-        return require __DIR__ . "/../src/app.php";
+        //return require __DIR__ . "/../src/app.php";
     }
     
     public function testHelloNameExample()
