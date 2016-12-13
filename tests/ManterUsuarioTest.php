52b8786340afc85b33ec92b02a4d82176855c9ef
Programador <Programador@10.41.1.129>
2016-12-13 14:48:29 -0200

    alterar para firefox

--- tests/ManterUsuarioTest.php
+++ tests/ManterUsuarioTest.php
@@ -25,7 +25,7 @@ class ManterUsuarioTest extends PHPUnit_Extensions_Selenium2TestCase {
         //$this->setBrowser('firefox');
         //$this->setHost('127.0.0.1');
         //$this->setPort(8896);
-        //$this->setBrowserUrl('http://www1.mastercase.net/');
+        $this->setBrowserUrl('http://www1.mastercase.net/');
         //$this->setDesiredCapabilities(['Browser'=>'firefox','platform'=>'ANY','version'=>'46.0']);
         //$this->ca
         
