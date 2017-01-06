f76ed819bd94ab317d6acb8eb9d86439bc490a3b
Programador <Programador@10.41.1.103>
2017-01-06 17:44:10 -0200

    alterar para firefox

--- tests/ManterUsuarioTest.php
+++ tests/ManterUsuarioTest.php
@@ -41,8 +41,8 @@ class ManterUsuarioTest extends PHPUnit_Extensions_Selenium2TestCase {
         $this->setBrowser('chrome');
         $this->setPort(4444);
         $this->setHost('jenkins.mastercase.net');
-        //$this->setBrowserUrl('http://jenkins.mastercase.net:4444/wd/hub');
-        $this->setBrowserUrl('http://www1.mastercase.net/');
+        $this->setBrowserUrl('http://jenkins.mastercase.net:4444/wd/hub');
+        //$this->setBrowserUrl('http://www1.mastercase.net/');
         //$this->setSeleniumServerRequestsTimeout(120000);
         //$this->set
         //print_r($b);
