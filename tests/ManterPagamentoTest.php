e91fcbc58a9135c7e344179b4a4cf292cc54b6f9
Programador <Programador@10.41.1.103>
2017-01-27 15:28:04 -0200

    alterar para firefox

--- tests/ManterPagamentoTest.php
+++ tests/ManterPagamentoTest.php
@@ -26,7 +26,7 @@ class ManterPagamentoteste  extends PHPUnit_Extensions_Selenium2TestCase {
            
     //put your code here
       protected function setUp() {
-        $this->setBrowser('firefox');
+        $this->setBrowser('chrome');
         $this->setPort(4444);
         $this->setBrowserUrl('http://www1.mastercase.net/');
    
