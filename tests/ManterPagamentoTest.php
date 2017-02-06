5d40f0ee2fe98bf4a4f7370eeef3b30095f28cdd
Programador <Programador@10.41.1.103>
2017-02-06 18:41:28 -0200

    adicionado cookies

--- tests/ManterPagamentoTest.php
+++ tests/ManterPagamentoTest.php
@@ -30,7 +30,7 @@ class ManterPagamentoteste  extends PHPUnit_Extensions_Selenium2TestCase {
           
         $this->setBrowser('chrome');
         //$this->setHost('10.41.1.131');
-        $this->setPort(8896);
+        //$this->setPort(8896);
         $this->setBrowserUrl('http://www1.mastercase.net/');
         //$this->setBrowserUrl('http://www.google.com.br/');
        } 
