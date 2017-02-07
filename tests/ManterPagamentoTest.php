67ddf9969f04df24e374d28b3491d4acf7899202
Programador <Programador@10.41.1.103>
2017-02-07 17:17:21 -0200

    adicionado cookies

--- tests/ManterPagamentoTest.php
+++ tests/ManterPagamentoTest.php
@@ -39,39 +39,40 @@ class ManterPagamentoteste  extends PHPUnit_Extensions_Selenium2TestCase {
     /**                                          
      * @test                                          
      */                                                       
-//    public function pagamentoBoleto() {
+    public function pagamentoBoleto() {
+        
+            //$teste = array();
+        
+        $session = $this->prepareSession();
+        $session->url('/sandbox/gestor_inscricao/web/');
+        //$session->
+        //$session->cookie()->;
+         //$session->$cookies->add($this->byId("siteuser-email"), "ricardo@mastercase.com.br");
+         $session->cookie()->add($this->byId("siteuser-email"), "ricardo@mastercase.com.br");
+         $session->byId("siteuser-email")->value("ricardo@mastercase.com.br");
+         //$session->cookie()->add($name, $value)->
+        // cookies
+        //$session->cookie()->remove('language_version');
+        //$session->cookie()->add('language_version', 'en')->set();
+        //$this->url('/');
+        //$session->cookie()->add($data) ;
+        //$session->url('/sandbox/gestor_inscricao/web/');
+        
+        //$this->url('http://www.google.com.br/');
+        //$this->url("/sandbox/gestor_inscricao/web/");
 //        
-//            //$teste = array();
-//        
-//        $session = $this->prepareSession();
-//        $session->url('/sandbox/gestor_inscricao/web/');
-//        //$session->
-//        //$session->cookie()->;
-//         //$session->$cookies->add($this->byId("siteuser-email"), "ricardo@mastercase.com.br");
-//         $session->cookie()->add($this->byId("siteuser-email"), "ricardo@mastercase.com.br");
-//         //$session->cookie()->add($name, $value)->
-//        // cookies
-//        //$session->cookie()->remove('language_version');
-//        //$session->cookie()->add('language_version', 'en')->set();
-//        //$this->url('/');
-//        //$session->cookie()->add($data) ;
-//        //$session->url('/sandbox/gestor_inscricao/web/');
-//        
-//        //$this->url('http://www.google.com.br/');
-//        //$this->url("/sandbox/gestor_inscricao/web/");
-////        
-////        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
-////        $this->byId("siteuser-senha")->value("!12teste");
-////        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
-////        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
-////        // ERROR: Caught exception [TypeError: value.replace is not a function]
-////        $this->byXPath("//button[@type='submit']")->click();
-////        $this->byCssSelector("button.btn.btn-primary")->click();
-////        $this->byId("TABELA_BOLETO")->click();
-////        $this->byId("gerar_boleto")->click();
-////        sleep(5);
-////        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[4]")->click();    
-//    }
+//        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
+//        $this->byId("siteuser-senha")->value("!12teste");
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
+//        // ERROR: Caught exception [TypeError: value.replace is not a function]
+//        $this->byXPath("//button[@type='submit']")->click();
+//        $this->byCssSelector("button.btn.btn-primary")->click();
+//        $this->byId("TABELA_BOLETO")->click();
+//        $this->byId("gerar_boleto")->click();
+//        sleep(5);
+//        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[4]")->click();    
+    }
      
     /**                                          
      * @test                                          
