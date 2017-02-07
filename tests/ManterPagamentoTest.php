1c16a4b209fb77e1a13ff88c8636e300e974abf9
Programador <Programador@10.41.1.103>
2017-02-07 16:49:48 -0200

    adicionado cookies

--- tests/ManterPagamentoTest.php
+++ tests/ManterPagamentoTest.php
@@ -40,111 +40,117 @@ class ManterPagamentoteste  extends PHPUnit_Extensions_Selenium2TestCase {
      */                                                       
     public function pagamentoBoleto() {
         $session = $this->prepareSession();
-        // cookies
-        $session->cookie()->remove('language_version');
-        $session->cookie()->add('language_version', 'en')->set();
-        //$this->url('/');
-        $session->cookie()->postCookie($data) ;
         $session->url('/sandbox/gestor_inscricao/web/');
+        //$session->
+        //$session->cookie()->;
+         //$session->$cookies->add($this->byId("siteuser-email"), "ricardo@mastercase.com.br");
+         $session->cookie()->add($this->byId("siteuser-email"), "ricardo@mastercase.com.br");
+         //$session->cookie()->add($name, $value)->
+        // cookies
+        //$session->cookie()->remove('language_version');
+        //$session->cookie()->add('language_version', 'en')->set();
+        //$this->url('/');
+        //$session->cookie()->add($data) ;
+        //$session->url('/sandbox/gestor_inscricao/web/');
         
         //$this->url('http://www.google.com.br/');
         //$this->url("/sandbox/gestor_inscricao/web/");
-        
-        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
-        $this->byId("siteuser-senha")->value("!12teste");
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
-        // ERROR: Caught exception [TypeError: value.replace is not a function]
-        $this->byXPath("//button[@type='submit']")->click();
-        $this->byCssSelector("button.btn.btn-primary")->click();
-        $this->byId("TABELA_BOLETO")->click();
-        $this->byId("gerar_boleto")->click();
-        sleep(5);
-        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[4]")->click();    
+//        
+//        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
+//        $this->byId("siteuser-senha")->value("!12teste");
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
+//        // ERROR: Caught exception [TypeError: value.replace is not a function]
+//        $this->byXPath("//button[@type='submit']")->click();
+//        $this->byCssSelector("button.btn.btn-primary")->click();
+//        $this->byId("TABELA_BOLETO")->click();
+//        $this->byId("gerar_boleto")->click();
+//        sleep(5);
+//        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[4]")->click();    
     }
      
-    /**                                          
-     * @test                                          
-     */                                                       
-    public function pagamentoBoletoDesconto() {
-        //testes
-        $this->url("/sandbox/gestor_inscricao/web/");
-        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
-        $this->byId("siteuser-senha")->value("!12teste");
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
-        // ERROR: Caught exception [TypeError: value.replace is not a function]
-        $this->byXPath("//button[@type='submit']")->click();
-        $this->byCssSelector("button.btn.btn-primary")->click();
-        $this->byName("cupom")->value("DESCONTO1000");
-        $this->byCssSelector("button.btn.btn-default")->click();
-        $this->byId("TABELA_BOLETO")->click();
-        $this->byId("gerar_boleto")->click();
-        sleep(5);
-        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[4]")->click();
-    }
-    
-    /**
-     * @test
-     */
-    public function consultarMinhasComprar() {
-        $this->url("/sandbox/gestor_inscricao/web/");
-        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
-        $this->byId("siteuser-senha")->value("!12teste");
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
-        // ERROR: Caught exception [TypeError: value.replace is not a function]
-        $this->byXPath("//button[@type='submit']")->click();
-        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[2]")->click();
-        $this->byXPath("//div[2]/div[1]/div[2]/ul/li[2]/a")->click();
-    }
-    
-    /**
-     * @test
-     */
-    public function alterarEndereco() {
-        $this->url("/sandbox/gestor_inscricao/web/");
-        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
-        $this->byId("siteuser-senha")->value("!12teste");
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
-        // ERROR: Caught exception [TypeError: value.replace is not a function]
-        $this->byXPath("//button[@type='submit']")->click();
-        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[2]")->click();
-        $this->byXPath("//div[2]/div[1]/div[2]/ul/li[2]/a")->click();
-        $this->byXPath("(//a[contains(text(),'Pagar agora')])[31]")->click();
-        $this->byCssSelector("button.btn.btn-primary")->click();
-        sleep(5);
-        $this->byName("enderecoEntregue")->click();
-        sleep(5);
-        $this->byXPath("//div[2]/div[3]/div/div/div[3]/div/form/div/div[1]/div[1]/div/input")->value("79031-007");
-        $this->byCssSelector("div.col-xs-12.clearfix > div.pull-right > button.btn.btn-primary")->click();
-        $this->byCssSelector("button.swal2-confirm.styled")->click();
-        sleep(5);
-        $this->byCssSelector("div.col-xs-12.clearfix > div.pull-right > button.btn.btn-primary")->click();
-        sleep(5);
-        $this->byCssSelector("button.swal2-confirm.styled")->click();
-    }
-    
-    /**
-     * @test
-     */
-    public function alterarRepresentate() {
-        $this->url("/sandbox/gestor_inscricao/web/");
-        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
-        $this->byId("siteuser-senha")->value("!12teste");
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
-        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
-        // ERROR: Caught exception [TypeError: value.replace is not a function]
-        $this->byXPath("//button[@type='submit']")->click();
-        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[2]")->click();
-        $this->byXPath("//div[2]/div[1]/div[2]/ul/li[2]/a")->click();
-        $this->byXPath("(//a[contains(text(),'Pagar agora')])[18]")->click();
-        sleep(5);
-        $this->byXPath("(//button[@type='button'])[3]")->click();
-        sleep(5);
-        $this->byXPath("//div[2]/div[4]/div/div/div[3]/div/form/div/div[1]/div/span/span[1]/span/span[2]/b")->click();
-        $this->byXPath("//span/span/span[2]/ul/li[4]")->click();
-        $this->byXPath("//div[2]/div[4]/div/div/div[3]/div/form/div/div[2]/div/button[2]")->click();
-    }
+//    /**                                          
+//     * @test                                          
+//     */                                                       
+//    public function pagamentoBoletoDesconto() {
+//        //testes
+//        $this->url("/sandbox/gestor_inscricao/web/");
+//        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
+//        $this->byId("siteuser-senha")->value("!12teste");
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
+//        // ERROR: Caught exception [TypeError: value.replace is not a function]
+//        $this->byXPath("//button[@type='submit']")->click();
+//        $this->byCssSelector("button.btn.btn-primary")->click();
+//        $this->byName("cupom")->value("DESCONTO1000");
+//        $this->byCssSelector("button.btn.btn-default")->click();
+//        $this->byId("TABELA_BOLETO")->click();
+//        $this->byId("gerar_boleto")->click();
+//        sleep(5);
+//        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[4]")->click();
+//    }
+//    
+//    /**
+//     * @test
+//     */
+//    public function consultarMinhasComprar() {
+//        $this->url("/sandbox/gestor_inscricao/web/");
+//        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
+//        $this->byId("siteuser-senha")->value("!12teste");
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
+//        // ERROR: Caught exception [TypeError: value.replace is not a function]
+//        $this->byXPath("//button[@type='submit']")->click();
+//        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[2]")->click();
+//        $this->byXPath("//div[2]/div[1]/div[2]/ul/li[2]/a")->click();
+//    }
+//    
+//    /**
+//     * @test
+//     */
+//    public function alterarEndereco() {
+//        $this->url("/sandbox/gestor_inscricao/web/");
+//        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
+//        $this->byId("siteuser-senha")->value("!12teste");
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
+//        // ERROR: Caught exception [TypeError: value.replace is not a function]
+//        $this->byXPath("//button[@type='submit']")->click();
+//        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[2]")->click();
+//        $this->byXPath("//div[2]/div[1]/div[2]/ul/li[2]/a")->click();
+//        $this->byXPath("(//a[contains(text(),'Pagar agora')])[31]")->click();
+//        $this->byCssSelector("button.btn.btn-primary")->click();
+//        sleep(5);
+//        $this->byName("enderecoEntregue")->click();
+//        sleep(5);
+//        $this->byXPath("//div[2]/div[3]/div/div/div[3]/div/form/div/div[1]/div[1]/div/input")->value("79031-007");
+//        $this->byCssSelector("div.col-xs-12.clearfix > div.pull-right > button.btn.btn-primary")->click();
+//        $this->byCssSelector("button.swal2-confirm.styled")->click();
+//        sleep(5);
+//        $this->byCssSelector("div.col-xs-12.clearfix > div.pull-right > button.btn.btn-primary")->click();
+//        sleep(5);
+//        $this->byCssSelector("button.swal2-confirm.styled")->click();
+//    }
+//    
+//    /**
+//     * @test
+//     */
+//    public function alterarRepresentate() {
+//        $this->url("/sandbox/gestor_inscricao/web/");
+//        $this->byId("siteuser-email")->value("ricardo@mastercase.com.br");
+//        $this->byId("siteuser-senha")->value("!12teste");
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select")->click();
+//        $this->byXPath("//div[1]/div/div/div[2]/div/div/form/select/option[4]")->click();
+//        // ERROR: Caught exception [TypeError: value.replace is not a function]
+//        $this->byXPath("//button[@type='submit']")->click();
+//        $this->byXPath("//div[1]/div/div/div/div/div[2]/a[2]")->click();
+//        $this->byXPath("//div[2]/div[1]/div[2]/ul/li[2]/a")->click();
+//        $this->byXPath("(//a[contains(text(),'Pagar agora')])[18]")->click();
+//        sleep(5);
+//        $this->byXPath("(//button[@type='button'])[3]")->click();
+//        sleep(5);
+//        $this->byXPath("//div[2]/div[4]/div/div/div[3]/div/form/div/div[1]/div/span/span[1]/span/span[2]/b")->click();
+//        $this->byXPath("//span/span/span[2]/ul/li[4]")->click();
+//        $this->byXPath("//div[2]/div[4]/div/div/div[3]/div/form/div/div[2]/div/button[2]")->click();
+//    }
 } 
