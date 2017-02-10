47a46042abfceb37556475cf0185a5200edb83d4
Programador <Programador@10.41.1.103>
2017-02-10 09:12:43 -0200

    adicionado cookies

--- tests/TestExtra.php
+++ tests/TestExtra.php
@@ -7,7 +7,7 @@ class TestExtra  extends PHPUnit_Extensions_Selenium2TestCase
      */
     public function setUp()
     {
-        $this->setBrowser('firefox');
+        $this->setBrowser('chrome');
         $this->setHost('127.0.0.1');
         $this->setPort(4444);
         $this->setBrowserUrl('http://www.extra.com.br/');
