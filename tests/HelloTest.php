a64d2ea1c8799d6aa107a7b191ce67117ca241b8
Programador <Programador@10.41.1.104>
2017-07-20 12:43:22 -0300

    jenkins

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -6,7 +6,7 @@ class HelloTest extends Silex\WebTestCase
     public function createApplication()
     {
         //return require __DIR__ . "/../src/app.php";
-        return require __DIR__ . "/../../../../../../src/app.php";
+        return require __DIR__ . "/../src/app.php";
     }
     
     public function testHelloNameExample()
